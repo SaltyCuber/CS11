05c2092318e68dd2da2d49aee7c845b1d5fae80a
Tape Dispenser <59711006+SaltyCuber@users.noreply.github.com>
2021-09-28 13:18:16 -0700

    fixed add and multiply

--- meth.py
+++ meth.py
@@ -1,7 +1,7 @@
 def multiply(b, c):
     a = 0
     while c != 0:
-        if modulo(c, 2) == 1:
+        if c & 1 == 1:
             a = add(a, b)
         b = b << 1
         c = c >> 1
@@ -19,13 +19,19 @@ def modulo(b, c):
         b = subtract(b, c)
     return b
 
-def add(b, c):
-    a = nor(nor(b, c), nor(nor(b, b), nor(c, c)))
-    d = (nor(nor(b, b), nor(c, c))) << 1
-    while d != 0:
-      temp = (nor(nor(b, b), nor(c, c))) << 1  
-      a = nor(nor(a, d), nor(nor(a, a), nor(d, d)))
-      d = temp
+def add(b, c, carryIn=0):
+    a = 0
+    for i in range((b.bit_length()+c.bit_length()) + 1):
+        bitB = b & 1
+        bitC = c & 1
+        bitD = bitB ^ bitC
+        carry = bitB & bitC
+        bitA = carryIn ^ bitD
+        carry = carry | (carryIn & bitD)
+        carryIn = carry
+        b = b >> 1
+        c = c >> 1
+        a = (a | (bitA << i))
     return a
 
 def subtract(b, c):
